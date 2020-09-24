3968988af4b4e568fd315ea81f1596bd9a400ba9
Oleh-Crash <olezhkanovikov29@gmail.com>
2020-09-24 12:57:16 +0000

    Add commant with /0, and%

--- JS/index.js
+++ JS/index.js
@@ -2,22 +2,32 @@
 //calck
 const display = document.querySelector('.display');
 
+//digits
 const digitButtons = document.querySelectorAll('.digits button');
 digitButtons.forEach( button => button.addEventListener('click', digitClicked));
-
 function digitClicked(ev){
    display.value += ev.target.innerText;
 }
 
-
+//opers
 const operButtons = document.querySelectorAll('.opers button');
 operButtons.forEach( button => button.addEventListener('click', operClicked));
-
 function operClicked(ev){
    display.value += ev.target.innerText;
 }
 
+
+
+//equals
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
     display.value = eval(display.value);
 });
+
+//reset form
+function logReset(event) {
+  log.textContent = "Form reset!";
+}
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('reset', logReset);
